7ada7a3435e62203283c7c3a2e9f5f5e50999466
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2022-10-30 03:09:59 -0300

    Adapt selection to the generalized selection comp

--- frontend/src/pages/events/SelectionPage.vue
+++ frontend/src/pages/events/SelectionPage.vue
@@ -5,7 +5,7 @@
   >
     <filterable-list :items="events">
       <template #default="{ item }">
-        <select-event-item :event="item" />
+        <select-event-item :event="item" @select="handleSelection" />
       </template>
     </filterable-list>
   </page-with-add>
@@ -21,9 +21,15 @@ import PageWithAdd from 'components/shared/pages/PageWithAdd.vue'
 import FilterableList from 'components/shared/filterable/FilterableList.vue'
 import SelectEventItem from 'components/listItems/SelectEventItem.vue'
 
-const { store, quasar } = environment()
+const { store, router, quasar } = environment()
 
 onMounted(() => pull(store, quasar, 'events/fetch'))
 
-const events = computed(() => store.getters['events/eventsOptions'])
+const events = computed(() => store.state.events.events)
+
+function handleSelection(event) {
+  store.dispatch('events/selectEvent', event.value)
+  quasar.notify('Evento seleccionado')
+  router.push({ name: 'home' })
+}
 </script>
