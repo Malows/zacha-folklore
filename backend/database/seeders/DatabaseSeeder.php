96f5aa95d82ebbcad59feedab93a52791261796c
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2022-12-05 00:47:28 -0300

    update data seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class,
             RolesAndPermissionsSeeder::class,
+            UserSeeder::class,
             EventSeeder::class,
             MenuSectionSeeder::class,
             MenuItemSeeder::class,
--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -28,19 +29,65 @@ class UserSeeder extends Seeder
             ],
         ];
 
-        if (DB::table('users')->count() < 2) {
+        $isTest = App::environment('testing');
+
+        $userAmounts = $isTest ? 3 : 2;
+
+        if ($isTest) {
+            $user[] = [
+                'name' => 'ticket',
+                'email' => 'ticket@email.com',
+                'password' => Hash::make('ticket'),
+            ];
+        }
+
+        if (DB::table('users')->count() < $userAmounts) {
             foreach ($users as $user) {
                 $this->checkOrCreateUser($user);
             }
+
+            $this->assignRoles();
         }
     }
 
+
+    /**
+     * Check if the user exist or create it
+     *
+     * @param  array $user
+     * @return void
+     */
     private function checkOrCreateUser($user)
     {
         $fetched = DB::table('users')->where('email', $user['email'])->first();
 
         if (! $fetched) {
             DB::table('users')->insert($user);
+        }
+    }
+
+    /**
+     * Assign roles to the users
+     *
+     * @return void
+     */
+    private function assignRoles() {
+        $users = User::all();
+
+        $roles = [
+            Role::findByName('admin', 'api'),
+            Role::findByName('manager', 'api'),
+            Role::findByName('ticket controller', 'api'),
+        ];
+
+        foreach ($user as $user) {
+            $role = match ($user->name) {
+                env('ADMIN_USER_NAME') => $roles[0],
+                env('COMMON_USER_NAME') => $roles[1],
+                default => $roles[2],
+            };
+
+            $user->assignRole($role);
         }
     }
 }
